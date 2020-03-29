caf6a1099b889bd568d980c5abf852b57d6ea2a0
Nikolay <nik.vagin1995@mail.ru>
2020-03-29 04:14:50 +0300

    rubric

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 
-
 # Create your models here.
 
 class Bb(models.Model):
-    title = models.CharField(max_length = 50, verdose_name="Товар")
-    content = models.TextField(null=True, blank=True, verdose_name="Описание товара"))
-    price = models.FloatField(null=True, blank=True, verdose_name="Цена товара"))
-    pablished = models.DateTimeField(auto_now_add=True, db_index=True, verdose_name="Время публикацыи"))
+    title = models.CharField(max_length = 50,)
+    content = models.TextField(null=True, blank=True,)
+    price = models.FloatField(null=True, blank=True,)
+    pablished = models.DateTimeField(auto_now_add=True, db_index=True,)
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT)
 
-class Meta:
-    verdose_name_pijral = "Объявьение"
-    verdose_name = "Объявление"
-    ordering = ['-published']
 
+    class Meta:
+        ordering = ['-pablished']
+
+
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
-from .models import Bb
-
+from .models import Bb, Rubric
+from django.views.generic.edit import CreateView
+from .forms import BbForm
 # Create your views here.
 
 
-def index(request): 
-    bbs = Bb.objects.order_by('-published')
-    return render (request, 'bboard/index.html', {'bbs':bbs} ) 
+def index(request):
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs':bbs, 'rubrics':rubrics}
+    return render(request, 'bboard/index.html', context)
 
+
+def by_rubric(request,rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs':bbs,'rubrics':rubrics, 'current_rubric':current_rubric, }
+    return render(request, 'bboard/by_rubric.html', content)
+
+
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = '/bboard/'
+
+    def get_context_date(self,**kwargs):
+       context = super().get_context_date(**kwargs)
+       context['rubrics'] = Rubric.objects.all()
+       return context
